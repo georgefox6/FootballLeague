52e14f6fdbf7f8b812545f083f7825fbc5368cec
Josh Gillott <j.gillott@hotmail.co.uk>
2019-09-17 22:48:38 +0100

    Manually added getSaveGameNames

--- Football/src/FootballLeagueBackend/FileHandler.java
+++ Football/src/FootballLeagueBackend/FileHandler.java
@@ -1,7 +1,11 @@
 package FootballLeagueBackend;
 
-import java.nio.file.*;
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class FileHandler {
 	public FileHandler() {
@@ -21,6 +25,19 @@ public class FileHandler {
 		String sourcePath = "Football/SaveGames/" + sourceTitle + ".db";
 		String destinationPath = "Football/SaveGames/" + destinationTitle + ".db";
 		copyFile(sourcePath, destinationPath);
+	}
+
+	public ArrayList<String> getSaveGameNames() {
+		String saveGamesDirectoryPath = "../../SaveGames/";
+		ArrayList<String> saveGameTitles = new ArrayList<String>();
+		File[] saveGamesTitlesRaw = new File(saveGamesDirectoryPath).listFiles();
+		for (File file : saveGamesTitlesRaw) {
+			if (file.isFile()) {
+				saveGameTitles.add(file.getName().split("\\.")[0]);
+				System.out.println(file.getName().split("\\.")[0]);
+			}
+		}
+		return saveGameTitles;
 	}
 
 	public static void main(String[] args) {
