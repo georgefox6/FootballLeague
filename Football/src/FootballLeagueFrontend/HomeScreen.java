9bed0c2a5913b87d9a6cf4f55ca37e79b288abf5
Josh Gillott <j.gillott@hotmail.co.uk>
2019-09-25 22:57:33 +0100

    Copied all NewGameScreenFX functionality into HomeScreen Refactored and have working. HomeScreen now is an Application with one primary stage on which there are two possible scenes. Namely homeScreenScene and newGameScreenScene.

--- Football/src/FootballLeagueFrontend/HomeScreen.java
+++ Football/src/FootballLeagueFrontend/HomeScreen.java
@@ -7,8 +7,13 @@ import javafx.event.ActionEvent;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
+
+import FootballLeagueBackend.FileHandler;
 
 public class HomeScreen extends Application {
 
@@ -20,6 +25,10 @@ public class HomeScreen extends Application {
 
 	 private void initUI(Stage stage) {
 
+	 	/////////////////////////////
+	 	//      Home Screen        //
+	 	/////////////////////////////
+
 	 	Button newGameButton = new Button();
 	 	Button loadGameButton = new Button();
 	 	Button settingsButton = new Button();
@@ -30,38 +39,80 @@ public class HomeScreen extends Application {
 	 	settingsButton.setText("Settings");
 	 	quitButton.setText("Quit");
 
+	 	VBox mainMenu = new VBox();
+	 	mainMenu.getChildren().addAll(newGameButton, loadGameButton, settingsButton, quitButton);
+	 	mainMenu.setSpacing(10);
+	 	mainMenu.setPadding(new Insets(25));
 
-	 	newGameButton.setOnAction((ActionEvent event) -> {
-	 		pressedNewGameButton();
+	 	BorderPane borderPaneHomeScreen = new BorderPane();
+	 	borderPaneHomeScreen.setCenter(mainMenu);
+
+	 	Scene homeScreenScene = new Scene(borderPaneHomeScreen, 280, 200);
+		homeScreenScene.getStylesheets().add("FootballLeagueFrontend/FootballLeagueThemeOne.css");
+
+		/////////////////////////////
+	 	//     NewGame Screen      //
+	 	/////////////////////////////
+		
+		TextField newGameName = new TextField();
+		Button backButton = new Button();
+		Button createGameButton = new Button();
+
+		backButton.setText("Back");
+		createGameButton.setText("Create game");
+
+		VBox newGameMenu = new VBox();
+		newGameMenu.getChildren().addAll(newGameName, backButton, createGameButton);
+		newGameMenu.setSpacing(10);
+		newGameMenu.setPadding(new Insets(25));
+
+		BorderPane borderPaneNewGameScreen = new BorderPane();
+	 	borderPaneNewGameScreen.setCenter(newGameMenu);
+
+	 	Scene newGameScreenScene = new Scene(borderPaneNewGameScreen, 280, 200);
+		newGameScreenScene.getStylesheets().add("FootballLeagueFrontend/FootballLeagueThemeOne.css");
+
+		
+		/////////////////////////////
+		//   Home Screen Actions   //
+		/////////////////////////////
+
+		newGameButton.setOnAction((ActionEvent event) -> {
+	 		pressedNewGameButton(stage, newGameScreenScene);
 	 	});
-	 	newGameButton.setOnAction((ActionEvent event) -> {
+	 	loadGameButton.setOnAction((ActionEvent event) -> {
 	 		pressedLoadGameButton();
 	 	});
-	 	newGameButton.setOnAction((ActionEvent event) -> {
+	 	settingsButton.setOnAction((ActionEvent event) -> {
 	 		pressedSettingsButton();
 	 	});
 	 	quitButton.setOnAction((ActionEvent event) -> {
 	 		pressedQuitButton();
 	 	});
 
-	 	VBox menu = new VBox();
-	 	menu.getChildren().addAll(newGameButton, loadGameButton, settingsButton, quitButton);
-	 	menu.setSpacing(10);
-	 	menu.setPadding(new Insets(25));
+	 	/////////////////////////////
+		// New Game Screen Actions //
+		/////////////////////////////
 
-	 	BorderPane borderPane = new BorderPane();
-	 	borderPane.setCenter(menu);
+	 	backButton.setOnAction((ActionEvent event) -> {
+			pressedBackButton(stage, homeScreenScene);
+		});
+		createGameButton.setOnAction((ActionEvent event) -> {
+			pressedCreateGameButton(newGameName.getText());
+		});
 
-	 	Scene scene = new Scene(borderPane, 280, 200);
-		scene.getStylesheets().add("FootballLeagueFrontend/FootballLeagueThemeOne.css");
+		/////////////////////////////
+		//    Initialize Stage     //
+		/////////////////////////////
 
 	 	stage.setTitle("Main menu");
-	 	stage.setScene(scene);
+	 	stage.setScene(homeScreenScene);
 	 	stage.show();
 	 }
 
-	 private void pressedNewGameButton() {
-
+	 private void pressedNewGameButton(Stage stage, Scene newGameScreenScene) {
+	 	stage.setTitle("New game");
+	 	stage.setScene(newGameScreenScene);
 	 }
 
 	 private void pressedLoadGameButton() {
@@ -75,6 +126,31 @@ public class HomeScreen extends Application {
 	 private void pressedQuitButton() {
 	 	// stage.close();
 	 }
+
+	 private void pressedCreateGameButton(String saveGameName) {
+		System.out.println("Create game");
+		try {
+			createNewGame(saveGameName);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void pressedBackButton(Stage stage, Scene homeScreenScene) {
+		System.out.println("Back");
+		stage.setTitle("Main menu");
+		stage.setScene(homeScreenScene);
+	}
+
+	private void createNewGame(String saveGameName) throws IOException {
+		FileHandler f = new FileHandler();
+		String mainGameName = "mainGame";
+		try {
+			f.copyDatabase(mainGameName, saveGameName);
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 	 public static void main(String[] args) {
 	 	launch(args);
