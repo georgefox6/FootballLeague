70df71db164cbbf833991d952973fb9567529b20
George Fox <georgefox1996@gmail.com>
2020-02-23 00:00:28 +0000

    Added database operations to Match and Tactic classes

--- Football/src/FootballLeagueBackend/Tactic.java
+++ Football/src/FootballLeagueBackend/Tactic.java
@@ -1,6 +1,10 @@
 package FootballLeagueBackend;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
+
+import static FootballLeagueBackend.DatabaseConnection.*;
 
 public class Tactic {
     //TODO tactic should probably have a name variable to users can name the tactics they create
@@ -41,6 +45,35 @@ public class Tactic {
 
 
     //Setters
+
+
+    public void setTacticCode(String tacticCode) {
+        this.tacticCode = tacticCode;
+    }
+
+    public void setStartingXICode(String startingXICode) {
+        this.startingXICode = startingXICode;
+    }
+
+    public void setAttackScore(double attackScore) {
+        this.attackScore = attackScore;
+    }
+
+    public void setDefenceScore(double defenceScore) {
+        this.defenceScore = defenceScore;
+    }
+
+    public void setFormation(String formation) {
+        this.formation = formation;
+    }
+
+    public void setPlayStyle(String playStyle) {
+        this.playStyle = playStyle;
+    }
+
+    public static void setCodeIteration(int codeIteration) {
+        Tactic.codeIteration = codeIteration;
+    }
 
     //Constructors
     Tactic(){}
@@ -84,4 +117,51 @@ public class Tactic {
         return StartingXI;
     }
     //TODO add function to calculate attack/defence scores based on the other inputs (players, formation and playstyle)
+
+    public static Tactic readTactic(String tacticCode){
+        ResultSet result = DatabaseConnection.readQuery("tactic", "tacticCode='" + tacticCode);
+        try {
+            assert result != null;
+            if(result.next()){
+                return new Tactic(tacticCode, result.getString("startingXICode"), result.getDouble("attackScore"), result.getDouble("defenceScore"), result.getString("formation"), result.getString("playStyle"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DatabaseConnection.close();
+        }
+        return null;
+    }
+
+    //For example use "WHERE formation='4-4-2'" to get tactics with the formation 4-4-2 or " " to get all tactics
+    public static ArrayList<Tactic> readAllTactics(String clause){
+        ArrayList<Tactic> tactics = new ArrayList<>();
+        try{
+            ResultSet rs = readAllQuery("tactic", clause);
+            assert rs != null;
+            while(rs.next()){
+                tactics.add(new Tactic(rs.getString("tacticCode"), rs.getString("startingXICode"), rs.getDouble("attackScore"), rs.getDouble("defenceScore"), rs.getString("formation"), rs.getString("playStyle")));
+            }
+            System.out.println("Tactic Size : " + tactics.size());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DatabaseConnection.close();
+        }
+        return tactics;
+    }
+
+    public static boolean writeTactic(Tactic tactic){
+        String values = String.format("'%s', '%s', '%s', '%s', '%s', '%s'", tactic.getTacticCode(), tactic.getStartingXICode(), tactic.getAttackScore(), tactic.getDefenceScore(), tactic.getFormation(), tactic.getPlayStyle());
+        return DatabaseConnection.writeQuery("tactic", values);
+    }
+
+    public static void updateTactic(Tactic tactic){
+        String values = String.format("startingXICode='%s', attackScore='%s', defenceScore='%s', formation='%s', playStyle='%s' WHERE tacticCode='%s'", tactic.getStartingXICode(), tactic.getAttackScore(), tactic.getDefenceScore(), tactic.getFormation(), tactic.getPlayStyle(), tactic.getTacticCode());
+        updateQuery("tactic", values);
+    }
+
+    public static int countMatch(){
+        return countQuery("tactic", "tacticCode");
+    }
 }
